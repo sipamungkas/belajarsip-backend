10947d9f0e84df2ca4dde115fb38b9d8db689cf8
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-03-28 06:17:24 +0700

    feat: add search params to courses end point

--- src/handlers/courses.js
+++ src/handlers/courses.js
@@ -3,7 +3,13 @@ const { sendError, sendResponse } = require("../helpers/response");
 
 const getCourses = async (req, res) => {
   try {
-    const courses = await coursesWithLevelAndCategory();
+    const { search, category, level } = req.query;
+    const searchValue = `%${search}%`;
+    const courses = await coursesWithLevelAndCategory(
+      searchValue,
+      category,
+      level
+    );
     return sendResponse(res, true, 200, "List of Available Courses", courses);
   } catch (error) {
     console.log(error);
--- src/models/courses.js
+++ src/models/courses.js
@@ -1,11 +1,18 @@
 const db = require("../database/dbMySql");
 
-const coursesWithLevelAndCategory = () => {
+const coursesWithLevelAndCategory = (searchValue, categoryId, levelId) => {
   return new Promise((resolve, reject) => {
     const findAllQuery =
-      "SELECT c.*, l.name as level, cat.name as category FROM courses c join course_levels l on c.level_id = l.id join categories cat on c.category_id = cat.id";
+      "SELECT c.*, l.name as level, cat.name as category FROM courses c join course_levels l on c.level_id = l.id" +
+      " join categories cat on c.category_id = cat.id join days d on c.day_id = d.id where c.name like ?";
+    const findByCategoryIdQuery = "and c.category_id = ?";
+    const findByLevelIdQuery = " and c.level_id = ?";
+    const sqlQuery = `${findAllQuery} ${
+      categoryId ? findByCategoryIdQuery : ""
+    } ${levelId ? findByLevelIdQuery : ""}`;
 
-    db.query(findAllQuery, (error, results) => {
+    db.query(sqlQuery, [searchValue, categoryId, levelId], (error, results) => {
+      console.log(results);
       if (error) return reject(error);
       return resolve(results);
     });
