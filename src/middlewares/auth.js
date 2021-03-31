ee046d4a743313598084e06350e24800d72a940d
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-03-31 23:50:53 +0700

    feat: add isAdmin and isStudent middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,10 +4,10 @@ const { sendResponse, sendError } = require("../helpers/response");
 const jwtSecret = process.env.JWT_SECRET;
 
 const authenticateToken = async (req, res, next) => {
-  if (!req.headers.authorization) {
-    return sendResponse(res, false, 401, "Unauthorized Access");
-  }
   try {
+    if (!req.headers.authorization) {
+      return sendResponse(res, false, 401, "Unauthorized Access");
+    }
     const { authorization } = req.headers;
     const bearer = authorization.split(" ");
     const token = bearer[1];
@@ -21,4 +21,36 @@ const authenticateToken = async (req, res, next) => {
   }
 };
 
-module.exports = { authenticateToken };
+const isInstructor = (req, res, next) => {
+  try {
+    if (!req.user) {
+      return sendResponse(res, false, 401, "Unauthorized Access");
+    }
+    const { user } = req;
+    if (user.role_id !== 1) {
+      if (!user) return sendResponse(res, false, 401, "Unauthorized Access");
+    }
+    return next();
+  } catch (error) {
+    console.log(error);
+    return sendError(res, error);
+  }
+};
+
+const isStudent = (req, res, next) => {
+  try {
+    if (!req.user) {
+      return sendResponse(res, false, 401, "Unauthorized Access");
+    }
+    const { user } = req;
+    if (user.role_id !== 2) {
+      if (!user) return sendResponse(res, false, 401, "Unauthorized Access");
+    }
+    return next();
+  } catch (error) {
+    console.log(error);
+    return sendError(res, error);
+  }
+};
+
+module.exports = { authenticateToken, isInstructor, isStudent };
