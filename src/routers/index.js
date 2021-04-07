49e07fcef06543d512a5e76ed78c27a7bf0329b6
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-04-07 23:19:34 +0700

    fix: remove cors package from index.js feat: add preflight handler to routers/index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -8,6 +8,17 @@ const { authenticateToken } = require("../middlewares/authentication");
 const categoriesRouter = require("./categories");
 const levelsRouter = require("./levels");
 
+Router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Authorization,Content-type");
+    return res.send(200);
+  }
+  next();
+  // res.send(200);
+});
+
 Router.get("/", (req, res) => {
   return res.status(200).send("pong");
 });
