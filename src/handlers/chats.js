096ede93aef2feb5f7a93976c4531de794ee64cc
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-06-08 06:44:17 +0700

    feat(chats): add console.log

--- src/handlers/chats.js
+++ src/handlers/chats.js
@@ -77,7 +77,7 @@ const sendMessage = async (req, res) => {
     const receiver = members.map(
       (member) => `msgNotification:${member.user_ud}`
     );
-
+    console.log(receiver);
     socket.sendMsgNotification(receiver, {
       title: "You got new Message!",
       content: `New Message from ${name}`,
